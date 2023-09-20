ba719897950441c49de4f83f4e36a202e6cbe30c
einarcarias <einarcarias@gmail.com>
2023-09-20 16:29:54 +0100

    added new functions that compare the averages

--- main.py
+++ main.py
@@ -12,8 +12,10 @@ import numpy as np
 class ExpPostProcess:
     data_dir = Path("data")
 
-    def __init__(self, filename):
+    def __init__(self, filename, aoa):
+        self.aoa = aoa
         os.chdir(self.data_dir)
+        self.filename = filename
         self.df = pd.read_csv(filename, delimiter="\t")
         os.chdir("..")
 
@@ -41,42 +43,83 @@ class ExpPostProcess:
     def plot_avg(self, force):
         dict = {"moment": 3, "lift": 1, "drag": 2}
         column = dict[force]
-        window = 100
-        smooth = self.df.iloc[:, column].rolling(window=window, center=True).mean()
-        smooth.fillna(0, inplace=True)
-        self.df[f"smooth {dict[force]}"] = smooth
-        min_time, max_time = 0.0460, 0.0645
-        relevant_data = (self.df.iloc[:, 0] >= min_time) & (
-            self.df.iloc[:, 0] <= max_time
-        )
-        indices = self.df[relevant_data].index
+        self.get_avg(force)
+        min_time, max_time = {
+            1: (0.0495, 0.0645),
+            2: (0.0460, 0.0645),
+            3: (0.0460, 0.0645),
+        }[column]
 
         # Plotting original and smoothed data
-        plt.figure(figsize=(10, 6))
+        plt.figure(figsize=(6, 3))
         plt.plot(
             self.df.iloc[:, 0],
             self.df.iloc[:, column],
             label="Original Data",
             color="blue",
         )
-        plt.plot(self.df.iloc[:, 0], smooth, label="Moving Average", color="red")
+        plt.plot(
+            self.df.iloc[:, 0],
+            self.df[f"smooth {dict[force]}"],
+            label="Moving Average",
+            color="red",
+        )
         plt.axvline(x=min_time, color="black", linestyle="--")
         plt.axvline(x=max_time, color="black", linestyle="--")
         plt.xlabel("Time")
         plt.ylabel("Value")
-        plt.title("Centered Moving Average Smoothing")
+        plt.title(f"Centered Moving Average Smoothing for {self.name()}")
 
         plt.legend()
-        plt.show()
+        plt.tight_layout()
+        plt.show(block=False)
+
+    def name(self):
+        bicone_data = {
+            0: ["TR001.csv", "TR007.csv", "TR008.csv"],
+            2: ["TR002.csv"],
+            4: ["TR003.csv"],
+            6: ["TR004.csv", "TR005.csv", "TR006.csv"],
+        }
+        fin_config_data = {
+            0: {0: "TR009.csv", 10: "TR012.csv"},
+            5: {0: "TR010.csv", 10: ["TR011.csv", "TR021.csv", "TR020.csv"]},
+        }
+        flap_config_data = {
+            0: {5: "TR013.csv", 17.5: "TR016.csv"},
+            5: {5: "TR014.csv", 17.5: ["TR015.csv", "TR017.csv", "TR019.csv"]},
+        }
+
+        for aoa, filenames in bicone_data.items():
+            if self.filename in filenames:
+                return f"AoA = {aoa}, Bicone"
+
+        for aoa, delta_data in fin_config_data.items():
+            for delta, filenames in delta_data.items():
+                filenames = filenames if isinstance(filenames, list) else [filenames]
+                if self.filename in filenames:
+                    return f"AoA = {aoa}, Delta = {delta} Fin"
+
+        for aoa, delta_data in flap_config_data.items():
+            for delta, filenames in delta_data.items():
+                filenames = filenames if isinstance(filenames, list) else [filenames]
+                if self.filename in filenames:
+                    return f"AoA = {aoa}, Delta = {delta} Flap"
+
+        return "Unknown"
 
     def get_avg(self, force):
         dict = {"moment": 3, "lift": 1, "drag": 2}
         column = dict[force]
-        window = 100
+        window = 50
         smooth = self.df.iloc[:, column].rolling(window=window, center=True).mean()
         smooth.fillna(0, inplace=True)
         self.df[f"smooth {dict[force]}"] = smooth
-        min_time, max_time = 0.0460, 0.0645
+        min_time, max_time = {
+            1: (0.0495, 0.0645) if self.aoa != 0 else (0.05, 0.07),
+            2: (0.0460, 0.0645),
+            3: (0.0460, 0.0645),
+        }[column]
         relevant_data = (self.df.iloc[:, 0] >= min_time) & (
             self.df.iloc[:, 0] <= max_time
         )
@@ -259,7 +302,10 @@ class PostPlots:
             ax.set_ylim(0, 0.3)
             ax.legend()
         fig.tight_layout()
-        plt.show()
+        plt.show(block=False)
+
+
+# functions
 
 
 def force_coef_std_calc(dict, force, force_std):
@@ -284,6 +330,65 @@ def force_repeat_avg_calc(force_list, force_std_list):
         * force_avg
     )
     return force_avg, force_std_avg
+
+
+def compare_avg_plot(*args, **kwargs):
+    for key, value in kwargs.items():
+        if key == "force":
+            force = value
+        elif key == "title":
+            title = value
+        elif key == "aoa":
+            aoa = value
+    # Create a figure and a grid of subplots
+    n_instances = len(args)
+    fig, axs = plt.subplots(n_instances, 1, figsize=(15, 5))
+
+    for i, obj in enumerate(args):
+        y_label = f"{force[i].capitalize()} Force (N)"
+        x_label = "Time(S)"
+        dict = {"moment": 3, "lift": 1, "drag": 2}
+        column = dict[force[i]]
+        min_time, max_time = {
+            1: (0.0495, 0.0645) if aoa[i] != 0 else (0.05, 0.07),
+            2: (0.0460, 0.0645),
+            3: (0.0460, 0.0645),
+        }[column]
+        obj.get_avg(force[i])
+        if not isinstance(obj, ExpPostProcess):
+            print(f"Skipping argument {i+1}, not a ExpPostProcess object.")
+            continue
+
+        if n_instances == 1:  # Special case when only one subplot
+            ax = axs
+        else:
+            ax = axs[i]
+
+        ax.plot(
+            obj.df.iloc[:, 0],
+            obj.df.iloc[:, column],
+            label="Original Data",
+            color="blue",
+        )
+        ax.plot(
+            obj.df.iloc[:, 0],
+            obj.df[f"smooth {dict[force[i]]}"],
+            color="red",
+            label="Moving Average",
+        )
+        ax.set_title(f"{title[i]}")
+        ax.axvline(x=min_time, color="black", linestyle="--")
+        ax.axvline(x=max_time, color="black", linestyle="--")
+        ax.set_xlabel(x_label)
+        ax.set_ylabel(y_label)
+        # Add major grid lines
+        ax.grid(which="major", linestyle="-", linewidth="0.5", color="black")
+
+        # Add minor grid lines
+        ax.minorticks_on()
+        ax.grid(which="minor", linestyle=":", linewidth="0.5", color="gray")
+    plt.tight_layout()
+    plt.show(block=False)
 
 
 class PostPlots_bcone(PostPlots):
@@ -361,11 +466,24 @@ if __name__ == "__main__":
         "diameter": (diameter, diameter_std),
     }
     # using moving average to smooth data
-    test = ExpPostProcess("TR009.csv")
-    test.plot_avg("drag")
-    test.plot_avg("lift")
-
+    # test = ExpPostProcess("TR009.csv")
+    # test.plot_avg("lift")
+    # for bicone_key in [0, 2, 4, 6]:
+    #     if type(bicone_data[bicone_key]) == list:
+    #         for file in bicone_data[bicone_key]:
+    #             print(file)
+    #             test = ExpPostProcess(file)
+    #             test.plot_avg("lift")
+    #     else:
+    #         test = ExpPostProcess(bicone_data[bicone_key])
+    #         test.plot_avg("lift")
     # test.find_fft("lift")
+    fins = [ExpPostProcess(file, 0) for file in fin_config_data[0].values()]
+    flaps = [ExpPostProcess(file, 0) for file in flap_config_data[0].values()]
+    title_fin = ["Fin, AoA = 0, Delta = 0", "Fin, AoA = 0, Delta = 10"]
+    title_flap = ["Flap, AoA = 0, Delta = 5", "Flap, AoA = 0, Delta = 17.5"]
+    compare_avg_plot(*fins, force=["lift", "lift"], title=title_fin, aoa=[0, 0])
+    compare_avg_plot(*flaps, force=["lift", "lift"], title=title_flap, aoa=[0, 0])
 
     # %% processing repeats
     # bicone
@@ -380,7 +498,7 @@ if __name__ == "__main__":
                 force_list = []
                 force_std_list = []
                 for file in bicone_data[key]:
-                    test = ExpPostProcess(file)
+                    test = ExpPostProcess(file, key)
                     force, force_std = test.get_avg(key_force)
                     force_list.append(force)
                     force_std_list.append(force_std)
@@ -396,7 +514,7 @@ if __name__ == "__main__":
             # if its not a list
             bicone_exp[key] = {}
             for key_force in ["drag", "lift", "moment"]:
-                test = ExpPostProcess(bicone_data[key])
+                test = ExpPostProcess(bicone_data[key], key)
                 force, force_std = test.get_avg(key_force)
                 force_coef, force_coef_std = force_coef_std_calc(
                     condition_dict, force, force_std
@@ -415,7 +533,7 @@ if __name__ == "__main__":
                     force_list = []
                     force_std_list = []
                     for file in fin_config_data[key][key_def]:
-                        test = ExpPostProcess(file)
+                        test = ExpPostProcess(file, key)
                         force, force_std = test.get_avg(key_force)
                         force_list.append(force)
                         force_std_list.append(force_std)
@@ -431,7 +549,7 @@ if __name__ == "__main__":
                 # if its not a list
                 fin_exp[key][key_def] = {}
                 for key_force in ["drag", "lift", "moment"]:
-                    test = ExpPostProcess(fin_config_data[key][key_def])
+                    test = ExpPostProcess(fin_config_data[key][key_def], key)
                     force, force_std = test.get_avg(key_force)
                     force_coef, force_coef_std = force_coef_std_calc(
                         condition_dict, force, force_std
@@ -449,7 +567,7 @@ if __name__ == "__main__":
                     force_list = []
                     force_std_list = []
                     for file in flap_config_data[key][key_def]:
-                        test = ExpPostProcess(file)
+                        test = ExpPostProcess(file, key)
                         force, force_std = test.get_avg(key_force)
                         force_list.append(force)
                         force_std_list.append(force_std)
@@ -465,7 +583,7 @@ if __name__ == "__main__":
                 # if its not a list
                 flap_exp[key][key_def] = {}
                 for key_force in ["drag", "lift", "moment"]:
-                    test = ExpPostProcess(flap_config_data[key][key_def])
+                    test = ExpPostProcess(flap_config_data[key][key_def], key)
                     force, force_std = test.get_avg(key_force)
                     force_coef, force_coef_std = force_coef_std_calc(
                         condition_dict, force, force_std
